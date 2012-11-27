5bc6f778dd20182b71850a9ddcd145a03cc52199
Jiho <Jiho@Jiho-PC>
2012-11-27 11:55:02 +0900

    ApexCustomLifelineDotLineCustomFigure의 hide될 구간이 등록되는 클래스 생성

---
+++ org.eclipse.papyrus.uml.diagram.sequence/src/org/eclipse/papyrus/uml/diagram/sequence/util/LifelineFigureHelper.java
@@ -0,0 +1,61 @@
+package org.eclipse.papyrus.uml.diagram.sequence.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.geometry.Rectangle;
+
+/**
+ * Lifeline의 show/hide 구간 저장
+ * @author Jiho
+ *
+ */
+public class LifelineFigureHelper {
+
+	static Map<IFigure, Collection<Rectangle>> hideRegions = new HashMap<IFigure, Collection<Rectangle>>();
+	
+	public static void showRegion(IFigure figure, Rectangle region) {
+		showRegion(figure, region, true);
+	}
+	
+	public static void showRegion(IFigure figure, Rectangle region, boolean isShow) {
+		if (figure == null || region == null)
+			return;
+		
+		Collection<Rectangle> regions = hideRegions.get(figure);
+		if (regions == null) {
+			hideRegions.put(figure, regions = new ArrayList<Rectangle>());
+		}
+		
+		if (!isShow) {
+			if (!regions.contains(region)) {
+				regions.add(region);
+			}
+		} else {
+			regions.remove(region);
+		}
+	}
+	
+	public static Collection<Rectangle> getHideRegions(IFigure figure) {
+		return hideRegions.get(figure);
+	}
+	
+	public static void showAllRegion(Rectangle region) {
+		Iterator<IFigure> iter = hideRegions.keySet().iterator();
+		while (iter.hasNext()) {
+			Collection<Rectangle> regions = hideRegions.get(iter.next());
+			if (regions.contains(region)) {
+				regions.remove(region);
+			}
+		}
+	}
+	
+	public static void removeAllRegion(IFigure figure) {
+		if (hideRegions.containsKey(figure))
+			hideRegions.remove(figure);
+	}
+}
