a8eb634ef0c8cee57a9b1518ac99f210baad0d2b
hanmomhanda <hanmomhanda@gmail.com>
2012-12-03 20:39:19 +0900

    ApexME 프로젝트 추가 시 refresh되게 구현

--- kr.co.apexsoft.modellipse.explorer/src/kr/co/apexsoft/modellipse/explorer/provider/ApexUMLContentProvider.java
+++ kr.co.apexsoft.modellipse.explorer/src/kr/co/apexsoft/modellipse/explorer/provider/ApexUMLContentProvider.java
@@ -14,32 +14,40 @@
 package kr.co.apexsoft.modellipse.explorer.provider;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import kr.co.apexsoft.modellipse.explorer.core.ApexModellipseProjectMap;
 import kr.co.apexsoft.modellipse.explorer.core.ApexProjectWrapper;
 
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.facet.infra.browser.uicore.CustomizableModelContentProvider;
 import org.eclipse.emf.facet.infra.browser.uicore.internal.AppearanceConfiguration;
-import org.eclipse.emf.facet.infra.browser.uicore.internal.model.ITreeElement;
 import org.eclipse.emf.facet.infra.browser.uicore.internal.model.ItemsFactory;
 import org.eclipse.emf.facet.infra.browser.uicore.internal.model.ModelElementItem;
+import org.eclipse.jface.viewers.AbstractTreeViewer;
+import org.eclipse.jface.viewers.StructuredViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.papyrus.editor.PapyrusMultiDiagramEditor;
-import org.eclipse.papyrus.infra.core.resource.ModelSet;
 import org.eclipse.papyrus.infra.core.resource.uml.UmlModel;
 import org.eclipse.papyrus.infra.core.services.ServicesRegistry;
 import org.eclipse.papyrus.infra.emf.Activator;
-import org.eclipse.papyrus.infra.emf.providers.MoDiscoContentProvider;
 import org.eclipse.papyrus.infra.onefile.utils.OneFileUtils;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.navigator.CommonViewer;
 import org.eclipse.uml2.uml.internal.impl.ModelImpl;
@@ -50,9 +58,9 @@ import org.eclipse.uml2.uml.internal.impl.ModelImpl;
  * @deprecated Use {@link kr.co.apexsoft.modellipse.explorer.provider.ApexUMLContentProvider.providers.UMLContentProvider} instead
  */
 @Deprecated
-public class ApexUMLContentProvider extends CustomizableModelContentProvider {
+public class ApexUMLContentProvider extends CustomizableModelContentProvider implements IResourceChangeListener {
 
-	private CommonViewer common;
+	private Viewer viewer;
 
 	private final AppearanceConfiguration appearanceConfiguration;	
 
@@ -60,6 +68,8 @@ public class ApexUMLContentProvider extends CustomizableModelContentProvider {
 //		super();
 		super(Activator.getDefault().getCustomizationManager());
 		appearanceConfiguration = new AppearanceConfiguration(new ItemsFactory());
+
+//		ResourcesPlugin.getWorkspace().addResourceChangeListener(this, IResourceChangeEvent.POST_CHANGE);
 	}	
 
 	/**
@@ -241,11 +251,32 @@ public class ApexUMLContentProvider extends CustomizableModelContentProvider {
 	}
 
 	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-
 		super.inputChanged(viewer, oldInput, newInput);
 
-		if(viewer instanceof CommonViewer) {
-			common = (CommonViewer)viewer;
+		this.viewer = viewer;
+		IWorkspace oldWorkspace = null;
+		IWorkspace newWorkspace = null;
+
+		if (oldInput instanceof IWorkspace) {
+			oldWorkspace = (IWorkspace) oldInput;
+		} else if (oldInput instanceof IContainer) {
+			oldWorkspace = ((IContainer) oldInput).getWorkspace();
+		}
+
+		if (newInput instanceof IWorkspace) {
+			newWorkspace = (IWorkspace) newInput;
+		} else if (newInput instanceof IContainer) {
+			newWorkspace = ((IContainer) newInput).getWorkspace();
+		}
+
+		if (oldWorkspace != newWorkspace) {
+			if (oldWorkspace != null) {
+				oldWorkspace.removeResourceChangeListener(this);
+			}
+			if (newWorkspace != null) {
+				newWorkspace.addResourceChangeListener(this,
+						IResourceChangeEvent.POST_CHANGE);
+			}
 		}
 	}
 
@@ -272,5 +303,242 @@ public class ApexUMLContentProvider extends CustomizableModelContentProvider {
 			UmlModel umlModel = aProjectWrapper.getUmlModel(diFilePath);
 			makeModelElementItemList(umlModel, result);
 		}	
+	}
+	
+	@Override
+	public void resourceChanged(IResourceChangeEvent event) {
+		processDelta(event.getDelta());
+	}
+	
+	/**
+	 * from WorkbenchContentProvider
+	 */
+	/**
+	 * Process the resource delta.
+	 * 
+	 * @param delta
+	 */
+	protected void processDelta(IResourceDelta delta) {		
+
+		Control ctrl = viewer.getControl();
+		if (ctrl == null || ctrl.isDisposed()) {
+			return;
+		}
+		
+		
+		final Collection runnables = new ArrayList();
+		processDelta(delta, runnables);
+
+		if (runnables.isEmpty()) {
+			return;
+		}
+
+		//Are we in the UIThread? If so spin it until we are done
+		if (ctrl.getDisplay().getThread() == Thread.currentThread()) {
+			runUpdates(runnables);
+		} else {
+			ctrl.getDisplay().asyncExec(new Runnable(){
+				/* (non-Javadoc)
+				 * @see java.lang.Runnable#run()
+				 */
+				public void run() {
+					//Abort if this happens after disposes
+					Control ctrl = viewer.getControl();
+					if (ctrl == null || ctrl.isDisposed()) {
+						return;
+					}
+					
+					runUpdates(runnables);
+				}
+			});
+		}
+
+	}
+
+	/**
+	 * Run all of the runnables that are the widget updates
+	 * @param runnables
+	 */
+	private void runUpdates(Collection runnables) {
+		Iterator runnableIterator = runnables.iterator();
+		while(runnableIterator.hasNext()){
+			((Runnable)runnableIterator.next()).run();
+		}
+		
+	}
+
+	/**
+	 * Process a resource delta. Add any runnables
+	 */
+	private void processDelta(IResourceDelta delta, Collection runnables) {
+		//he widget may have been destroyed
+		// by the time this is run. Check for this and do nothing if so.
+		Control ctrl = viewer.getControl();
+		if (ctrl == null || ctrl.isDisposed()) {
+			return;
+		}
+
+		// Get the affected resource
+		final IResource resource = delta.getResource();
+	
+		// If any children have changed type, just do a full refresh of this
+		// parent,
+		// since a simple update on such children won't work,
+		// and trying to map the change to a remove and add is too dicey.
+		// The case is: folder A renamed to existing file B, answering yes to
+		// overwrite B.
+		IResourceDelta[] affectedChildren = delta
+				.getAffectedChildren(IResourceDelta.CHANGED);
+		for (int i = 0; i < affectedChildren.length; i++) {
+			if ((affectedChildren[i].getFlags() & IResourceDelta.TYPE) != 0) {
+				runnables.add(getRefreshRunnable(resource));
+				return;
+			}
+		}
+
+		// Opening a project just affects icon, but we need to refresh when
+		// a project is closed because if child items have not yet been created
+		// in the tree we still need to update the item's children
+		int changeFlags = delta.getFlags();
+		if ((changeFlags & IResourceDelta.OPEN) != 0) {
+			if (resource.isAccessible())  {
+				runnables.add(getUpdateRunnable(resource));
+			} else {
+				runnables.add(getRefreshRunnable(resource));
+				return;
+			}
+		}
+		// Check the flags for changes the Navigator cares about.
+		// See ResourceLabelProvider for the aspects it cares about.
+		// Notice we don't care about F_CONTENT or F_MARKERS currently.
+		if ((changeFlags & (IResourceDelta.SYNC
+				| IResourceDelta.TYPE | IResourceDelta.DESCRIPTION)) != 0) {
+			runnables.add(getUpdateRunnable(resource));
+		}
+		// Replacing a resource may affect its label and its children
+		if ((changeFlags & IResourceDelta.REPLACED) != 0) {
+			runnables.add(getRefreshRunnable(resource));
+			return;
+		}
+
+		// Handle changed children .
+		for (int i = 0; i < affectedChildren.length; i++) {
+			processDelta(affectedChildren[i], runnables);
+		}
+
+		// @issue several problems here:
+		//  - should process removals before additions, to avoid multiple equal
+		// elements in viewer
+		//   - Kim: processing removals before additions was the indirect cause of
+		// 44081 and its varients
+		//   - Nick: no delta should have an add and a remove on the same element,
+		// so processing adds first is probably OK
+		//  - using setRedraw will cause extra flashiness
+		//  - setRedraw is used even for simple changes
+		//  - to avoid seeing a rename in two stages, should turn redraw on/off
+		// around combined removal and addition
+		//   - Kim: done, and only in the case of a rename (both remove and add
+		// changes in one delta).
+
+		IResourceDelta[] addedChildren = delta
+				.getAffectedChildren(IResourceDelta.ADDED);
+		IResourceDelta[] removedChildren = delta
+				.getAffectedChildren(IResourceDelta.REMOVED);
+
+		if (addedChildren.length == 0 && removedChildren.length == 0) {
+			return;
+		}
+
+		final Object[] addedObjects;
+		final Object[] removedObjects;
+
+		// Process additions before removals as to not cause selection
+		// preservation prior to new objects being added
+		// Handle added children. Issue one update for all insertions.
+		int numMovedFrom = 0;
+		int numMovedTo = 0;
+		if (addedChildren.length > 0) {
+			addedObjects = new Object[addedChildren.length];
+			for (int i = 0; i < addedChildren.length; i++) {
+				addedObjects[i] = addedChildren[i].getResource();
+				if ((addedChildren[i].getFlags() & IResourceDelta.MOVED_FROM) != 0) {
+					++numMovedFrom;
+				}
+			}
+		} else {
+			addedObjects = new Object[0];
+		}
+
+		// Handle removed children. Issue one update for all removals.
+		if (removedChildren.length > 0) {
+			removedObjects = new Object[removedChildren.length];
+			for (int i = 0; i < removedChildren.length; i++) {
+				removedObjects[i] = removedChildren[i].getResource();
+				if ((removedChildren[i].getFlags() & IResourceDelta.MOVED_TO) != 0) {
+					++numMovedTo;
+				}
+			}
+		} else {
+			removedObjects = new Object[0];
+		}
+		// heuristic test for items moving within same folder (i.e. renames)
+		final boolean hasRename = numMovedFrom > 0 && numMovedTo > 0;
+		
+		Runnable addAndRemove = new Runnable(){
+			public void run() {
+				if (viewer instanceof AbstractTreeViewer) {
+					AbstractTreeViewer treeViewer = (AbstractTreeViewer) viewer;
+					// Disable redraw until the operation is finished so we don't
+					// get a flash of both the new and old item (in the case of
+					// rename)
+					// Only do this if we're both adding and removing files (the
+					// rename case)
+					if (hasRename) {
+						treeViewer.getControl().setRedraw(false);
+					}
+					try {
+						if (addedObjects.length > 0) {
+							treeViewer.add(resource, addedObjects);
+						}
+						if (removedObjects.length > 0) {
+							treeViewer.remove(removedObjects);
+						}
+					}
+					finally {
+						if (hasRename) {
+							treeViewer.getControl().setRedraw(true);
+						}
+					}
+				} else {
+					((StructuredViewer) viewer).refresh(resource);
+				}
+			}
+		};
+		runnables.add(addAndRemove);
+	}
+	/**
+	 * Return a runnable for refreshing a resource.
+	 * @param resource
+	 * @return Runnable
+	 */
+	private Runnable getRefreshRunnable(final IResource resource) {
+		return new Runnable(){
+			public void run() {
+				((StructuredViewer) viewer).refresh(resource);
+			}
+		};
+	}
+
+		/**
+		 * Return a runnable for refreshing a resource.
+		 * @param resource
+		 * @return Runnable
+		 */
+	private Runnable getUpdateRunnable(final IResource resource) {
+		return new Runnable(){
+			public void run() {
+				((StructuredViewer) viewer).update(resource, null);
+			}
+		};
 	}
 }
---
+++ kr.co.apexsoft.modellipse.explorer/src/kr/co/apexsoft/modellipse/explorer/wizard/ApexNewProjectWizard.java
@@ -0,0 +1,12 @@
+package kr.co.apexsoft.modellipse.explorer.wizard;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.papyrus.uml.diagram.wizards.Activator;
+import org.eclipse.papyrus.uml.diagram.wizards.Messages;
+import org.eclipse.papyrus.uml.diagram.wizards.NewPapyrusProjectWizard;
+
+public class ApexNewProjectWizard extends NewPapyrusProjectWizard {
+
+	// TODO : 현재 NewPapyrusProjectWizard를 사용하고 있으나 추후 Custom Wizard로 바꿔 사용
+}
